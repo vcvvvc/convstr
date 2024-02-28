53a2d19324e58c46c50d3ec2c6cb54829a4f1d06
Vc <vcvckw@gmail.com>
2024-02-28 17:39:19 +0800

    add

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ function removeSpace(text){
     return text.replace(/\s/g, "");
 }
 
+function toLowText(text){
+    if(typeof text !== "string") throw new TypeError("Need text is string");
+    return text.toLowerCase();
+}
+
+function toSha256(text) {
+    if(typeof text !== "string") throw new TypeError("Need text is string");
+    let hash = crypto.createHash('sha256');
+    hash.update(text);
+
+    return hash.digest('hex');
+}
+
+function toMd5(text) {
+    if(typeof text !== "string") throw new TypeError("Need text is string");
+
+    let hash = crypto.createHash('md5');
+    hash.update(text);
+
+    return hash.digest('hex');
+}
+
 function encryptedStr(text) {
     if(typeof text !== "string") throw new TypeError("Need text is string");
 
@@ -20,8 +42,17 @@ function encryptedStr(text) {
     return encrypt.encrypt(text);
 }
 
+function toTimestamp(text) {
+    if(typeof text !== "string") throw new TypeError("Need text is string");
+
+    return Date.parse(dateString);
+}
 
 module.exports = {
     removeSpace: removeSpace,
-    encryptedStr: encryptedStr,
+    toLowText: toLowText,
+    toSha256: toSha256,
+    toMd5: toMd5,
+    toTimestamp: toTimestamp,
+    encryptedStr: encryptedStr
 };
