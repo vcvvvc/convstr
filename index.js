52a72a734075afa4d78c6f82a44447fab86b402b
Vc <vcvckw@gmail.com>
2024-02-29 17:52:52 +0800

    add

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const crypto = require('crypto')
 var JSEncrypt = require('jsencrypt').default;
+var sub_modules = {};
+
+module.exports = function(sub_module_name) {
+    var sub_module = sub_modules[sub_module_name];
+    if(!sub_module) {
+        try {
+            sub_module = require("./sub_modules/"+sub_module_name);
+            sub_modules[sub_module_name] = sub_module;
+        } catch(e) {
+            throw new Error("Invalid sub_module name: "+sub_module_name);
+        }
+    }
+    return sub_module;
+};
 
 function removeSpace(text){
     if(typeof text !== "string") throw new TypeError("Need text is string");
